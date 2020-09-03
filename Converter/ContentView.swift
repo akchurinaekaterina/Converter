d171bf8f85b94fa5a1d6f5eec4a491a7d86a8aac
Ekaterina Akchurina <akchurina.ekaterina@gmail.com>
2020-09-03 19:47:09 +0300

    Initial Commit

---
+++ Converter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Converter
+//
+//  Created by Ekaterina Akchurina on 03.09.2020.
+//  Copyright © 2020 Ekaterina Akchurina. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
